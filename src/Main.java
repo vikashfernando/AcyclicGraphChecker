a61b4503ec1c31898b5dc55c45ca6a5159b7890b
vikashfernando <vikashdeshitha@gmail.com>
2026-03-31 05:53:05 +0530

    main class added

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+    public static void main(String[] args) {
+
+        // Change file name here to test
+        String fileName = "input2.txt";
+
+        DirectedGraph graph = GraphFileReader.readGraph(fileName);
+
+        System.out.println("Checking graph...");
+
+        boolean isAcyclic = AcyclicChecker.checkAcyclic(graph);
+
+        if (isAcyclic) {
+            System.out.println("Graph is ACYCLIC (YES)");
+        } else {
+            System.out.println("Graph has a CYCLE (NO)");
+            AcyclicChecker.printCycle(graph);
+        }
+    }
 }
