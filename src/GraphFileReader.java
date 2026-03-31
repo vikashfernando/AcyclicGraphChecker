2bb96dcdcb10120981701cdc243f9067f4351158
vikashfernando <vikashdeshitha@gmail.com>
2026-03-31 05:54:13 +0530

    update GraphFileReader.java

--- src/GraphFileReader.java
+++ src/GraphFileReader.java
@@ -1,2 +1,28 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class GraphFileReader {
+
+    public static DirectedGraph readGraph(String fileName) {
+        DirectedGraph graph = new DirectedGraph();
+
+        try {
+            File file = new File(fileName);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextInt()) {
+                int from = scanner.nextInt();
+                int to = scanner.nextInt();
+
+                graph.addEdge(from, to);
+            }
+
+            scanner.close();
+
+        } catch (Exception e) {
+            System.out.println("Error reading file");
+        }
+
+        return graph;
+    }
 }
